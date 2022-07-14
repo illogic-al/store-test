4c534acbdfd6ba1b9810b2ec65ed63d58d4ef048
Orville Bennett <dev@thebennettproject.com>
2022-07-14 11:03:07 -0400

    Add component that uses $app/stores.

---
+++ src/components/Header.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+	import { page } from '$app/stores';
+</script>
+
+<header>
+  <a class:active={$page.url.pathname === '/'} class="navbar-item" href="/"> Home </a>
+</header>
+
+
+<style lang="css">
+  .active {color: red}
+</style>
